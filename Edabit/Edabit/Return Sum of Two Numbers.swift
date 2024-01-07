22e67e5a052956f6f75c12748013483200dcc78a
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-01-07 20:09:13 +0100

    Add Return the sum of two numbers

---
+++ Edabit/Edabit/Return Sum of Two Numbers.swift
@@ -0,0 +1,15 @@
+//
+//  Return Sum of Two Numbers.swift
+//  Edabit
+//
+//  Created by Michal Pietrzak on 07/01/2024.
+//
+
+import Foundation
+
+struct SumOfTwoNumbers {
+    func add(_ a: Int, _ b: Int) -> Int {
+        a + b
+    }
+}
+
---
+++ Edabit/Edabit/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Edabit
+//
+//  Created by Michal Pietrzak on 07/01/2024.
+//
+
+import Foundation
+
+let sumOfTwoNumbers = SumOfTwoNumbers()
+print(sumOfTwoNumbers.add(3, 5))
