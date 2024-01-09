bc8df8565ffb419e866077b172d4d603d044a2ee
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-01-09 12:14:12 +0100

    Add Area Of a Triangle

---
+++ Edabit/Edabit/Area of a Triangle.swift
@@ -0,0 +1,17 @@
+//
+//  Area of a Triangle.swift
+//  Edabit
+//
+//  Created by Michal Pietrzak on 09/01/2024.
+//
+
+import Foundation
+
+// Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+struct AreaOfTriangle {
+    func calcArea(_ base: Int, _ height: Int) -> Int {
+        (base * height) / 2
+    }
+}
--- Edabit/Edabit/main.swift
+++ Edabit/Edabit/main.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 let sumOfTwoNumbers = SumOfTwoNumbers()
-print(sumOfTwoNumbers.add(3, 5))
+//print(sumOfTwoNumbers.add(3, 5))
 
 let convertAgeToDays = ConvertAgeToDays()
-print(convertAgeToDays.convertAge(65))
+//print(convertAgeToDays.convertAge(65))
+
+let areaOfTriangle = AreaOfTriangle()
+print(areaOfTriangle.calcArea(10, 10))
