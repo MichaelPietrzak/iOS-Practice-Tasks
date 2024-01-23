60893dfe3f055d72efe80551e4a4e89389ade307
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-01-23 14:23:53 +0100

    Modify: Are the numbers if equal file

---
+++ Edabit/Edabit/Are the number if equal.swift
@@ -0,0 +1,16 @@
+//  Are the numbers if equal.swift
+//  Edabit
+//
+//  Created by Michal Pietrzak on 15/01/2024.
+//
+
+import Foundation
+
+// Are the Numbers Equal?
+// Create a function that takes two integers and checks if they are equal.
+
+struct NumbersIfEqual {
+    func isEqual(_ num1: Int, _ num2: Int) -> Bool {
+        num1 == num2 ? true : false
+    }
+}
