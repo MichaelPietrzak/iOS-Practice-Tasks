cb02476ef95b702d7e85b02bff85d42447401054
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-02-06 13:21:41 +0100

    feat: nth even number

---
+++ Edabit/Edabit/Nth even number.swift
@@ -0,0 +1,17 @@
+//
+//  Nth even number.swift
+//  Edabit
+//
+//  Created by Michal Pietrzak on 06/02/2024.
+//
+
+import Foundation
+
+// Nth Even Number
+// Create a function that takes a number n and returns the nth even number beginning with 0 as the first.
+
+struct NthEvenNum {
+    func nthEven(_ n: Int) -> Int {
+         2 * n - 2
+    }
+}
